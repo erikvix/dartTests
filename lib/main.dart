40de1d0d9bb5a67e9250a33db5f8cf39e94554b7
jusu <erikvixx@gmail.com>
2024-11-15 11:45:30 -0300

    feat: init files structure

---
+++ lib/main.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:app2/theme/colorTheme.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomePage(title: 'Home Page'),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.gray900,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many tims:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
---
+++ lib/theme/colorTheme.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Tailwind CSS Colors
+  static const Color blue50 = Color(0xFFE0F2FE);
+  static const Color blue100 = Color(0xFFBAE6FD);
+  static const Color blue200 = Color(0xFF7DD3FC);
+  static const Color blue300 = Color(0xFF38BDF8);
+  static const Color blue400 = Color(0xFF0EA5E9);
+  static const Color blue500 = Color(0xFF0284C7);
+  static const Color blue600 = Color(0xFF0369A1);
+  static const Color blue700 = Color(0xFF075985);
+  static const Color blue800 = Color(0xFF0C4A6E);
+  static const Color blue900 = Color(0xFF1E3A8A);
+
+  static const Color gray50 = Color(0xFFF9FAFB);
+  static const Color gray100 = Color(0xFFF3F4F6);
+  static const Color gray200 = Color(0xFFE5E7EB);
+  static const Color gray300 = Color(0xFFD1D5DB);
+  static const Color gray400 = Color(0xFF9CA3AF);
+  static const Color gray500 = Color(0xFF6B7280);
+  static const Color gray600 = Color(0xFF4B5563);
+  static const Color gray700 = Color(0xFF374151);
+  static const Color gray800 = Color(0xFF1F2937);
+  static const Color gray900 = Color(0xFF111827);
+
+  static const Color red500 = Color(0xFFF87171);
+  static const Color green500 = Color(0xFF34D399);
+  static const Color yellow500 = Color(0xFFFACC15);
+  static const Color purple500 = Color(0xFF9B4D96);
+}
